adc6707ecbf1ba2d756f75431c11abce6518a8a5
imukulsaini <77503907+imukulsaini@users.noreply.github.com>
2022-02-04 17:34:53 +0530

    remove files

--- my-app/src/app/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
-import userReducer from '../../features/User/userSlice';
-export const store = configureStore({
-  reducer: {
-    users : userReducer
-  },
-});

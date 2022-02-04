5d2c5f9a343e2d69a470582a975f996a33dffed3
imukulsaini <77503907+imukulsaini@users.noreply.github.com>
2022-02-04 17:51:28 +0530

    feat add Profile Page components

---
+++ src/pages/Profile/components/index.js
@@ -0,0 +1,8 @@
+export { ProfileName } from "./ProfileName";
+export { ProfilePostCount } from "./ProfilePostCount";
+export { ProfilePostRoutes } from "./ProfilePostRoutes";
+export { ProfileUserAvatar } from "./ProfileUserAvatar";
+export { ProfileUserBio } from "./ProfileUserBio";
+export { ProfileUserFollowers } from "./ProfileUserFollowers";
+export { ProfileUserFollowing } from "./ProfileUserFollowing";
+export { ProfileUserMessageButton } from "./ProfileUserMessageButton";

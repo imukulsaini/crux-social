f8608fcb15cd2873dac023c4957b7a0eb0e0ba47
imukulsaini <77503907+imukulsaini@users.noreply.github.com>
2022-02-04 17:49:21 +0530

    add pages index file

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+export { AccountSettings } from "./Account/Account";
+export { PasswordChange } from "./Account/components/PasswordChange";
+export { DeleteUserAccount } from "./Account/components/DeleteAccount";
+export { ProfileSettings } from "./Account/components/ProfileSettings";
+export { UserPostsShow } from "./components/UserPostsShow/UserPostsShow";
+export { UserBookmarkShow } from "./components/UserBookMarkShow/UserBookmarkShow";
+export { UserLikesShow } from "./components/UserLikesShow/UserLikesShow";
+export { NoMatch } from "./components/NoMatch/NoMatch";
+export { Home } from "./Home/Home";
+export { Messages } from "./Messages/Messages";
+export { PostID } from "./PostID/PostID";
+export { ProfilePage } from "./Profile/Profile";
+export { Search } from "./Search/Search";
+export { SignIn } from "./Signin/Signin";
+export { SignUp } from "./Signup/Signup";
+export {Notification} from "./Notification/Notification";
+
